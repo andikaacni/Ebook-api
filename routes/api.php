04216e6e3bd7898eed92448c486209e17e73b72b
Andika Catur <andikacatur321@gmail.com>
2021-01-17 17:13:25 +0700

    init project

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('user-profile', 'AuthController@userProfile');
+});
+
+Route::get('books', 'BookController@index');
+Route::post('books', 'BookController@store');
+Route::get('books/{id}  ', 'BookController@show');
+Route::put('books/{id}', 'BookController@update');
+Route::delete('books/{id}', 'BookController@destroy');
+
+Route::get('authors','AuthorController@index');
+Route::post('authors/create', 'AuthorController@store');
+Route::delete('authors/{id}', 'AuthorController@destroy');
+Route::put('athors/{id}', 'AuthorController@destroy');
+Route::post('authors/{id}', 'AuthorController@update');
